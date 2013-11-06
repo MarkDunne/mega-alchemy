d1ff5c12dab452bbde8da760a332b7ac67a6e40a
Mark Dunne <mark.dunne02@gmail.com>
2013-11-06 01:17:03 +0000

    info done

--- client/main-page.js
+++ client/main-page.js
@@ -1,3 +1,5 @@
+Meteor.subscribe("elements");
+
 Meteor.startup(function(){
 
 	$.each(['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f', '#000', '#fff'], function() {
@@ -20,7 +22,11 @@ Meteor.startup(function(){
     	$('#icon_canvas').sketch().clear();
 		$('#element_name').val("");
     	$('#new_element_dialog_wrapper').removeClass('active');
-    })
+    });
+
+    $('#element_info_wrapper').click(function(){
+    	$('#element_info_wrapper').removeClass('active');
+    });
 });
 
 new_element = function(parents){
@@ -45,6 +51,30 @@ Template.elements.element = function(){
 Template.elements.rendered = function () {
 
 	var latest_clone; 
+
+	$('#elements .element').click(function(){
+		$('#element_info_wrapper').addClass('active');
+		element = Elements.find({_id: $(this).data('element-id')}).fetch()[0];
+		$('#element_info_wrapper #child p').text(element.name);
+		$('#element_info_wrapper #child img').attr("src", element.image);
+
+		parent1 = Elements.find({_id: element.parents[0]}).fetch()[0];
+		if(parent1){
+			$('#element_info_wrapper #parent1 p').text(parent1.name);
+			$('#element_info_wrapper #parent1 img').attr("src", parent1.image);			
+		}else{
+			$('#element_info_wrapper #parent1 p').text("");
+			$('#element_info_wrapper #parent1 img').attr("src", "");	
+		}
+		parent2 = Elements.find({_id: element.parents[1]}).fetch()[0];
+		if(parent2){
+			$('#element_info_wrapper #parent2 p').text(parent2.name);
+			$('#element_info_wrapper #parent2 img').attr("src", parent2.image);
+		}else{
+			$('#element_info_wrapper #parent2 p').text("");
+			$('#element_info_wrapper #parent2 img').attr("src", "");
+		}
+	});
 
 	$('.element').draggable({
 		helper: 'clone',
